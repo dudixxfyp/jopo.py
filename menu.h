cc10b158b331b6a25b8d2ddb78986428a5936d8d
Maria Eduarda <Mariaeduardma22@gmail.com>
2025-11-26 09:25:45 -0300

    Update menu.h to improve comments and includes

--- menu.h
+++ menu.h
@@ -1,9 +1,9 @@
 #ifndef MENU_H
 #define MENU_H
 
-#include "game.h"   // expõe hero, ghost, game_score, map[][], e variáveis globais
+#include "game.h"   // importa pacman, ghosts[], constantes, variáveis globais
 
-// Pilha para histórico de ações
+// ------------------ PILHA ------------------
 typedef struct Node {
     char acao;
     struct Node *prox;
@@ -13,16 +13,15 @@ typedef struct {
     Node *topo;
 } Pilha;
 
-// pilha
+// Funções da pilha
 void initPilha(Pilha *p);
 void push(Pilha *p, char acao);
 char pop(Pilha *p);
 int pilhaVazia(Pilha *p);
 
-// menu e I/O
+// ------------------ MENU E SALVAMENTO ------------------
+int salvarJogo();
+int carregarJogo();
 void mostrarMenu();
-int salvarJogo();     // salva os dados globais (usa externs de game.h)
-int carregarJogo();   // carrega os dados e atualiza as variáveis globais
 
 #endif // MENU_H
-
